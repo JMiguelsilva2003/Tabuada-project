ca57e4bb877090d8105b94e47189bb50f83b8663
JMiguelsilva2003 <jmigueldasilva953@gmail.com>
2024-06-04 16:36:33 -0300

    versão 1.0
    site tabuada feito.

---
+++ Scripts/script.js
@@ -0,0 +1,18 @@
+function tabuada(){
+    let num = document.getElementById("txtn");
+    let tab = document.getElementById("seltab");
+    let res = document.getElementById("res");
+    if(num.value.length == 0) {
+        alert("Por favor, digite um número!");
+    }else{
+        let n = Number(num.value)
+        tab.innerHTML = "";
+        for(let c = 1; c <= 10; c++){
+            let item = document.createElement("option");
+            item.text = `${n} x ${c} = ${n*c}`;
+            item.value = `tab${c}`;
+            tab.appendChild(item);
+        }
+        res.innerHTML = `A tabuada de ${n} está pronta!`;
+    }
+}
